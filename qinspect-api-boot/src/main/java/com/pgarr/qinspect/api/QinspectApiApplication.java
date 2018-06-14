721db3b3148a043ad012070d63fff06bd6622879
Piotr <34452081+pgarr@users.noreply.github.com>
2018-06-14 09:56:50 +0200

    spring boot project template

---
+++ qinspect-api-boot/src/main/java/com/pgarr/qinspect/api/QinspectApiApplication.java
@@ -0,0 +1,12 @@
+package com.pgarr.qinspect.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QinspectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QinspectApiApplication.class, args);
+	}
+}
